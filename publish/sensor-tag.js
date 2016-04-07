af59932bca093490d22589f6aa4739b4dbc50e5f
Fabijan Spralja <fabijan@spralja.hr>
2016-04-07 17:01:00 +0200

    error calbacks on feature enable, error var on value callbacks

--- publish/sensor-tag.js
+++ publish/sensor-tag.js
@@ -193,19 +193,19 @@ function monitorSensorTag(client) {
 	});
 
 	function initAirSensors() {
-		device.enableIrTemperature();
-		device.enableHumidity();
-		device.enableBarometricPressure();
-		device.enableLuxometer();
+		device.enableIrTemperature(function(err) {if (err) throw err;});
+		device.enableHumidity(function(err) {if (err) throw err;});
+		device.enableBarometricPressure(function(err) {if (err) throw err;});
+		device.enableLuxometer(function(err) {if (err) throw err;});
 		var intervalId = setInterval(function() {
 		  if(!connected) {
 		  	clearInterval(intervalId);
 		  	return;
 		  }
-		  device.readLuxometer(function(lux) {
-			  device.readBarometricPressure(function(pressure) {
-				device.readHumidity(function(temperature, humidity) {
-				  device.readIrTemperature(function(objectTemperature, ambientTemperature) {
+		  device.readLuxometer(function(error, lux) {
+			  device.readBarometricPressure(function(error, pressure) {
+				device.readHumidity(function(error, temperature, humidity) {
+				  device.readIrTemperature(function(error, objectTemperature, ambientTemperature) {
 					var data = {
 					   "d": {
 						 "myName": "TI Sensor Tag",
